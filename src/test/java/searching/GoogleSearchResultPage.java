3e24c8e1b918a65604a0266dce2e11f4f7e926ec
Serhii Nesterov <110775549+Serhiinv@users.noreply.github.com>
2024-09-27 11:41:51 +0300

    Update GoogleSearchResultPage.java

--- src/test/java/searching/GoogleSearchResultPage.java
+++ src/test/java/searching/GoogleSearchResultPage.java
@@ -1,17 +1,1 @@
-package searching;
 
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Selenide;
-
-import static com.codeborne.selenide.Condition.text;
-
-public class GoogleSearchResultPage {
-    public void verifySearchResultAtList(int expectedResultAmount) {
-        Selenide.$$("h3")
-                .shouldHave(CollectionCondition.sizeGreaterThanOrEqual(expectedResultAmount));
-    }
-
-    public void verifySearchResultLinkNumberContain(int linkNumber, String term) {
-        Selenide.$$("h3").get(linkNumber - 1).shouldHave(text(term));
-    }
-}
